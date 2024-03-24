92ea1eb9710367b713f97690bfccd6c7f660e231
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-03-24 14:56:45 +0100

    feat: bimage convert

--- ResourceFileEditor/Manager/Image/BimageFile.cs
+++ ResourceFileEditor/Manager/Image/BimageFile.cs
@@ -14,52 +14,55 @@ namespace ResourceFileEditor.Manager.Image;
 /// <param name="images">The images of the bimage file.</param>
 public readonly struct BimageFile(DateTime timeStamp, TextureType textureType, TextureFormat textureFormat, ColorFormat colorFormat, uint width, uint height, Bimage[] images)
 {
-	private const uint IMAGE_FILE_MAGIC = ('B' << 0) | ('I' << 8) | ('M' << 16) | (10 << 24);
-
 	/// <summary>
-	/// The timestamp of the bimage file.
+	/// The version of the bimage file.
 	/// </summary>
-	public DateTime TimeStamp { get; } = timeStamp;
+	public const int Version = 10;
 
 	/// <summary>
 	/// The "Magic" of the bimage file.
 	/// </summary>
-	public static uint Magic => IMAGE_FILE_MAGIC;
+	public const uint Magic = ('B' << 0) | ('I' << 8) | ('M' << 16) | (Version << 24);
+
+	/// <summary>
+	/// The timestamp of the bimage file.
+	/// </summary>
+	public DateTime TimeStamp => timeStamp;
 
 	/// <summary>
 	/// The texture type of the bimage file.
 	/// </summary>
-	public TextureType TextureType { get; } = textureType;
+	public TextureType TextureType => textureType;
 
 	/// <summary>
 	/// The texture format of the bimage file.
 	/// </summary>
-	public TextureFormat TextureFormat { get; } = textureFormat;
+	public TextureFormat TextureFormat => textureFormat;
 
 	/// <summary>
 	/// The color format of the bimage file.
 	/// </summary>
-	public ColorFormat ColorFormat { get; } = colorFormat;
+	public ColorFormat ColorFormat => colorFormat;
 
 	/// <summary>
 	/// The width of the bimage file.
 	/// </summary>
-	public uint Width { get; } = width;
+	public uint Width => width;
 
 	/// <summary>
 	/// The height of the bimage file.
 	/// </summary>
-	public uint Height { get; } = height;
+	public uint Height => height;
 
 	/// <summary>
 	/// The levels of the bimage file.
 	/// </summary>
-	public uint Levels { get; } = (uint)images.Length;
+	public uint Levels => (uint)images.Length;
 
 	/// <summary>
 	/// The images of the bimage file.
 	/// </summary>
-	public Bimage[] Images { get; } = images;
+	public Bimage[] Images => images;
 }
 
 /// <summary>
@@ -76,30 +79,119 @@ public readonly struct Bimage(uint level, uint destZ, uint width, uint height, u
 	/// <summary>
 	/// The level of the image.
 	/// </summary>
-	public uint Level { get; } = level;
+	public uint Level => level;
 
 	/// <summary>
 	/// The destination of the image.
 	/// </summary>
-	public uint DestZ { get; } = destZ;
+	public uint DestZ => destZ;
 
 	/// <summary>
 	/// The width of the image.
 	/// </summary>
-	public uint Width { get; } = width;
+	public uint Width => width;
 
 	/// <summary>
 	/// The height of the image.
 	/// </summary>
-	public uint Height { get; } = height;
+	public uint Height => height;
 
 	/// <summary>
 	/// The size of the image.
 	/// </summary>
-	public uint Size { get; } = size;
+	public uint Size => size;
 
 	/// <summary>
 	/// The date of the image.
 	/// </summary>
-	public byte[] Data { get; } = data;
+	public byte[] Data => data;
+}
+
+/// <summary>
+/// The bimage type enumerator.
+/// </summary>
+public enum BImageType : uint
+{
+	Disabled,
+	TwoD,
+	Cubic
+}
+
+/// <summary>
+/// The bimage format enumerator.
+/// </summary>
+public enum BImageFormat : uint
+{
+	None,
+
+	//------------------------
+	// Standard color image formats
+	//------------------------
+
+	RGBA8,      // 32 bpp
+	XRGB8,      // 32 bpp
+
+	//------------------------
+	// Alpha channel only
+	//------------------------
+
+	// Alpha ends up being the same as L8A8 in our current implementation, because straight 
+	// alpha gives 0 for color, but we want 1.
+	Alpha,
+
+	//------------------------
+	// Luminance replicates the value across RGB with a constant A of 255
+	// Intensity replicates the value across RGBA
+	//------------------------
+
+	L8A8,     // 16 bpp
+	Luminance8,   //  8 bpp
+	Intensity8,   //  8 bpp
+
+	//------------------------
+	// Compressed texture formats
+	//------------------------
+
+	Dxt1,     // 4 bpp
+	Dxt5,     // 8 bpp
+
+	//------------------------
+	// Depth buffer formats
+	//------------------------
+
+	Depth,      // 24 bpp
+
+	//------------------------
+	//
+	//------------------------
+
+	X16,      // 16 bpp
+	Y16X16,     // 32 bpp
+	RGB565      // 16 bpp
+}
+
+/// <summary>
+/// The bimage color format enumerator.
+/// </summary>
+public enum BImageColorFormat : uint
+{
+	/// <summary>
+	/// RGBA.
+	/// </summary>
+	Default,
+
+	/// <summary>
+	/// XY format and use the fast DXT5 compressor.
+	/// </summary>
+	NormalDxt5,
+
+	/// <summary>
+	/// Convert RGBA to CoCg_Y format.
+	/// </summary>
+	YCoCgDxt5,
+
+	/// <summary>
+	/// Copy the alpha channel to green.
+	/// </summary>
+	GreenAlpha
 }
